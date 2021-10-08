b437c4892657ab1e28ffa4416812613b41feac7d
Leandro Antunes <lele.tune@gmail.com>
2021-10-07 21:41:11 -0300

    utilizando o useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import { RepositoryList } from './Components/RepositoryList';
-import { Counter } from './Components/Counter';
 import './styles/global.scss';
 
 export function App() {
     return(
-        <>
-            <RepositoryList />
-            <Counter />
-        </>
+        <RepositoryList />
     )
 
 }
--- src/Components/RepositoryList.jsx
+++ src/Components/RepositoryList.jsx
@@ -1,4 +1,9 @@
+import { useEffect, useState } from "react";
 import { RepositoryItem } from "./RepositoryItem";
+
+import '../styles/repositories.scss'
+
+// https://api.github.com/orgs/rocketseat/repos
 
 const repository = {
     name: 'unform',
@@ -7,6 +12,14 @@ const repository = {
 }
 
 export function RepositoryList(){
+    const [repositories, setRepositories] = useState([]) // esse estado vai amarzanar uma lista de repositórios 
+
+    useEffect(() => {
+        fetch('https://api.github.com/orgs/rocketseat/repos') // busca a API de repositórios 
+        .then(Response => Response.json())                    // converter esse dados em arquivo json
+        .then(data => setRepositories(data))                  // mostra esses repositórios
+    },[])
+
     return (
         <section className="repository-list">
             <h1>Lista de repositórios</h1>
