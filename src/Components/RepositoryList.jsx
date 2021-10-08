3137f45e0e3f819a3bb0a6e08d1b3fff5ad41b89
Leandro Antunes <lele.tune@gmail.com>
2021-10-08 11:50:24 -0300

    Listando repositorios

--- src/Components/RepositoryList.jsx
+++ src/Components/RepositoryList.jsx
@@ -3,14 +3,6 @@ import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss'
 
-// https://api.github.com/orgs/rocketseat/repos
-
-const repository = {
-    name: 'unform',
-    description: 'Form in React',
-    link: 'http://github.com/unform/unform'
-}
-
 export function RepositoryList(){
     const [repositories, setRepositories] = useState([]) // esse estado vai amarzanar uma lista de repositórios 
 
@@ -25,10 +17,9 @@ export function RepositoryList(){
             <h1>Lista de repositórios</h1>
 
             <ul>
-               <RepositoryItem repository={repository} />
-               <RepositoryItem repository={repository} />
-               <RepositoryItem repository={repository} />
-               <RepositoryItem repository={repository} />
+                {repositories.map(repository => {
+                    return <RepositoryItem key={repository.name} repository={repository} />
+                })}
             </ul>
         </section>
     )
